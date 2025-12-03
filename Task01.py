6688f262f619bff54e1a252105999e51b1d7c87e
Ashima Mittal <ashimamittal1806@gmail.com>
2025-12-04 01:31:56 +0530

    Add files via upload
    I have uploaded the assignment 1 which has two tasks first one is to take input from user and apply some mathematical operations on it and second one is take input of name and greet him

---
+++ Task01.py
@@ -0,0 +1,14 @@
+a = float(input("Enter the First number:"))
+b = float(input("Enter the Second number:"))
+
+Addition = a + b
+print("Addition is = ", Addition)
+
+Subtraction = a - b 
+print("Subtraction is = ", Subtraction)
+
+Multiplication = a * b
+print("Multiplication is = ", Multiplication)
+
+Division = a / b
+print("Division is = ", Division)
